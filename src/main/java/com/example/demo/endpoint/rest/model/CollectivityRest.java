a0ed7fefe1adab1174cd770d98adf56e9dc44c4c
Mahefa04 <hei.mahefa.2@gmail.com>
2026-04-21 21:49:25 +0300

    feat: add CollectivityRest in model

--- src/main/java/com/example/demo/endpoint/rest/model/CollectivityRest.java
+++ src/main/java/com/example/demo/endpoint/rest/model/CollectivityRest.java
@@ -1,4 +1,52 @@
 package com.example.demo.endpoint.rest.model;
 
+import java.util.List;
+
 public class CollectivityRest {
+    private String id;
+    private String location;
+    private CollectivityStructureRest structure;
+    private List<MemberRest> members;
+
+    public CollectivityRest() {
+    }
+
+    public CollectivityRest(String id, String location, CollectivityStructureRest structure, List<MemberRest> members) {
+        this.id = id;
+        this.location = location;
+        this.structure = structure;
+        this.members = members;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public CollectivityStructureRest getStructure() {
+        return structure;
+    }
+
+    public void setStructure(CollectivityStructureRest structure) {
+        this.structure = structure;
+    }
+
+    public List<MemberRest> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<MemberRest> members) {
+        this.members = members;
+    }
 }
