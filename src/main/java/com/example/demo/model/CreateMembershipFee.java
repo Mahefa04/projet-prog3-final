7264c6c40d58cdc55a43b18b660936d1f3a366a0
Mahefa04 <hei.mahefa.2@gmail.com>
2026-04-22 18:51:36 +0300

    feat: add import java.time.LocalDate; class

---
+++ src/main/java/com/example/demo/model/CreateMembershipFee.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+import jdk.jfr.Frequency;
+
+import java.time.LocalDate;
+
+public class CreateMembershipFee {
+    private LocalDate eligibleFrom;
+    private Frequency frequency;
+    private double amount;
+    private String label;
+
+    public LocalDate getEligibleFrom() {
+        return eligibleFrom;
+    }
+
+    public void setEligibleFrom(LocalDate eligibleFrom) {
+        this.eligibleFrom = eligibleFrom;
+    }
+
+    public Frequency getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(Frequency frequency) {
+        this.frequency = frequency;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
