b40baf9ab5bf3fa6e0e4680edca27cf1657ffba3
Mahefa04 <hei.mahefa.2@gmail.com>
2026-04-22 18:19:39 +0300

    feat: add CollectivityInformation class

---
+++ src/main/java/com/example/demo/model/CollectivityInformation.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+public class CollectivityInformation {
+    private String name;
+    private Integer number;
+
+    public CollectivityInformation(String name, Integer number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
