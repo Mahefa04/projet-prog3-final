e3449c541f1a1cea9dea67ca782cdd0b273bbddf
Mahefa04 <hei.mahefa.2@gmail.com>
2026-04-23 10:37:18 +0300

    feat: add -

--- Database/schemal.sql
+++ Database/schemal.sql
@@ -88,3 +88,27 @@ ON member_referees(member_id);
 
 CREATE INDEX idx_member_referees_referee_id
 ON member_referees(referee_id);
+
+CREATE TABLE financial_account (
+    id VARCHAR(50) PRIMARY KEY,
+    collectivity_id INTEGER NOT NULL,
+    account_type VARCHAR(30) NOT NULL,
+    holder_name VARCHAR(100),
+    mobile_banking_service VARCHAR(30),
+    mobile_number VARCHAR(20),
+    bank_name VARCHAR(50),
+    bank_code INTEGER,
+    bank_branch_code INTEGER,
+    bank_account_number INTEGER,
+    bank_account_key INTEGER,
+    FOREIGN KEY (collectivity_id) REFERENCES collectivities(id)
+);
+
+CREATE TABLE financial_account_transaction (
+    id VARCHAR(50) PRIMARY KEY,
+    financial_account_id VARCHAR(50) NOT NULL,
+    amount NUMERIC(12,2) NOT NULL,
+    transaction_type VARCHAR(10) NOT NULL,
+    creation_date DATE NOT NULL,
+    FOREIGN KEY (financial_account_id) REFERENCES financial_account(id)
+);
