a43cfa69cad30ed69d11868949fc7b23c24d7909
Mahefa04 <hei.mahefa.2@gmail.com>
2026-05-06 16:16:58 +0300

    fix: modification of StatisticsController class

--- src/main/java/com/example/demo/endpoint/rest/controller/StatisticsController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/StatisticsController.java
@@ -1,14 +1,15 @@
 package com.example.demo.endpoint.rest.controller;
 
+import com.example.demo.model.Statistics;
 import com.example.demo.service.StatisticsService;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/collectivities")
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
@@ -17,11 +18,11 @@ public class StatisticsController {
         this.statisticsService = statisticsService;
     }
 
-    @GetMapping("/statistics")
-    public List<Map<String, Object>> getAllStatistics(
+    @GetMapping("/collectivities/statistics")
+    public List<Statistics> getGlobalStatistics(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
-        
-        return statisticsService.getAllStatistics(from, to);
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
+    ) {
+        return statisticsService.getGlobalStatistics(from, to);
     }
 }
